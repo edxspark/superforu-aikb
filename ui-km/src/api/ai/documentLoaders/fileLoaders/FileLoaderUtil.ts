ed39cde8f2faa885f1812ce639ce09ee7ee5f711
moks.mo <moks.mo@edxspark.com>
2025-08-07 20:26:45 +0800

    remove ui-km.

--- ui-km/src/api/ai/documentLoaders/fileLoaders/FileLoaderUtil.ts
+++
@@ -1,8 +0,0 @@
-export interface FileLoaderParams {
-    type: string;
-    filePath: string;
-}
-
-export function fileLoader(params: FileLoaderParams) {
-    console.log("fileLoader....");
-}
--- ui-km/src/config/website.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 全局配置文件
- */
-
-const spfFileServer =
-  'http://superforu-bucket.oss-cn-shanghai.aliyuncs.com/superforu';
-
-export default spfFileServer;
